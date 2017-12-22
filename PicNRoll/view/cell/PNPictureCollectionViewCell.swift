544f39b5abce91d608448ae0ba5bfab83a89a019
Diana <botnaru.diana@yahoo.com>
2017-12-22 17:21:33 +0200

    added PNPictureCollectionViewCell, added ImageslideShow pod

---
+++ PicNRoll/view/cell/PNPictureCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  PNPictureCollectionViewCell.swift
+//  PicNRoll
+//
+//  Created by new on 12/22/17.
+//  Copyright © 2017 test. All rights reserved.
+//
+
+import UIKit
+
+class PNPictureCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+}
