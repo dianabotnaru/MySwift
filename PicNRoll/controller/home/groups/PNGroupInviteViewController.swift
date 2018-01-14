18d2ac35cd4930066cbdbcf4bddd6d471da77d3c
Diana <botnaru.diana@yahoo.com>
2018-01-14 03:28:47 +0200

    added PNNotification Data Model

--- PicNRoll/controller/home/groups/PNGroupInviteViewController.swift
+++ PicNRoll/controller/home/groups/PNGroupInviteViewController.swift
@@ -10,7 +10,6 @@ import UIKit
 import MessageUI
 import SVProgressHUD
 
-
 class PNGroupInviteViewController: PNFriendContactsViewController {
 
     var phoneNumbers : [String] = []
@@ -28,7 +27,6 @@ class PNGroupInviteViewController: PNFriendContactsViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-    
 
     func getInviteAvailableUserList(){
         var availableFriendList : [PNUser] = []
@@ -73,8 +71,3 @@ class PNGroupInviteViewController: PNFriendContactsViewController {
         }
     }
 }
-
-
-
-
-
---
+++ PicNRoll/model/PNNotification.swift
@@ -0,0 +1,24 @@
+//
+//  PNNotification.swift
+//  PicNRoll
+//
+//  Created by diana on 1/14/18.
+//  Copyright © 2018 test. All rights reserved.
+//
+
+import UIKit
+
+class PNNotification {
+    
+    var id: String = ""
+    var kind: Int = 0
+    var vendorId: String = ""
+    var vendorName:String = ""
+    var folderId:String  = ""
+    var folderName: String = ""
+    var groupId: String = ""
+    var groupName: String = ""
+    var createdDate: String = ""
+    var vendorImageUrl: String = ""
+    
+}
