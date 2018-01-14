f393a684f63c2ac5985e2c49719aeea2f112f374
Diana <botnaru.diana@yahoo.com>
2018-01-14 03:58:42 +0200

    added a function to set label with multiple fonts in notification page

--- PicNRoll/view/cell/PNNotificationTableViewCell.swift
+++ PicNRoll/view/cell/PNNotificationTableViewCell.swift
@@ -9,6 +9,12 @@
 import UIKit
 
 class PNNotificationTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var vendorImageView: UIImageView!
+    
+    @IBOutlet weak var decLabel: UILabel!
+
+    @IBOutlet weak var dateLabel: UILabel!
 
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -18,4 +24,18 @@ class PNNotificationTableViewCell: UITableViewCell {
         super.setSelected(selected, animated: animated)
     }
     
+    func setLabels(){
+        let attrs1 = [NSAttributedStringKey.font : UIFont.boldSystemFont(ofSize: 14), NSAttributedStringKey.foregroundColor : PNGlobal.PNDarkGrayColor]
+        let attrs2 = [NSAttributedStringKey.font : UIFont.systemFont(ofSize: 14), NSAttributedStringKey.foregroundColor : PNGlobal.PNDarkGrayColor]
+        
+        let attributedString1 = NSMutableAttributedString(string:"Daniel Ramadan", attributes:attrs1)
+        let attributedString2 = NSMutableAttributedString(string:" shared ", attributes:attrs2)
+        let attributedString3 = NSMutableAttributedString(string:"Wedding Partner", attributes:attrs1)
+        let attributedString4 = NSMutableAttributedString(string:" folder", attributes:attrs2)
+
+        attributedString1.append(attributedString2)
+        attributedString1.append(attributedString3)
+        attributedString1.append(attributedString4)
+        self.decLabel.attributedText = attributedString1
+    }
 }
