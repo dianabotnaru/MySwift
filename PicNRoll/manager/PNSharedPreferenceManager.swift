fca3ccb4c4a5dcf33a8d76dacac4220fe9758ac6
Diana <botnaru.diana@yahoo.com>
2018-01-16 05:36:02 +0200

    added PNSharedPreferenceManager

---
+++ PicNRoll/manager/PNSharedPreferenceManager.swift
@@ -0,0 +1,14 @@
+//
+//  PNSharedPreferenceManager.swift
+//  PicNRoll
+//
+//  Created by new on 1/16/18.
+//  Copyright © 2018 test. All rights reserved.
+//
+
+import UIKit
+
+class PNSharedPreferenceManager {
+    static let shared = PNSharedPreferenceManager()
+
+}
